dcd5f2d06bacc4acebf36f020dfdcaff51b9fc18
SiwonHwang <sight41siyah@gmail.com>
2022-09-30 23:23:40 +0900

    미디어 기기 open

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import http from "http";
+import browserEnv from "browser-env";
 
 const app = express();
 app.set("view engine", "pug");
@@ -7,7 +8,9 @@ app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 const server = http.createServer(app);
+
 const connectHandler = () => {
   console.log(`Listening on http://localhost:3000`);
 };
+
 server.listen(3000, connectHandler);
--- public/index.js
+++ public/index.js
@@ -1,4 +1,26 @@
-const video = document.querySelector("video");
+const myVideo = document.querySelector("#myVideo");
+
+const openMediaDevices = async (constraints) => {
+  return await navigator.mediaDevices.getUserMedia(constraints);
+};
+
+async function playVideoFromCamera() {
+  try {
+    const constraints = { video: true, audio: true };
+    const stream = openMediaDevices(constraints);
+    const video = myVideo.querySelector("video#localCamera");
+    console.log(stream);
+    console.log(video);
+    video.srcObject = stream;
+  } catch (e) {
+    console.error(`Error accessing media devices. ${e}`);
+  }
+}
+playVideoFromCamera();
+
+myVideo.addEventListener("click", () => {
+  console.log("video");
+});
 const btn = document.querySelector("button");
 btn.addEventListener("click", () => {
   console.log("it is a btn");
