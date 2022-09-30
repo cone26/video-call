a846227da4a536f840849e9cb29dde05b031f5eb
SiwonHwang <sight41siyah@gmail.com>
2022-09-30 22:37:15 +0900

    CSS 작업

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 const server = http.createServer(app);
-
-server.listen(3000, () => {
-  console.log("hi");
-});
+const connectHandler = () => {
+  console.log(`Listening on http://localhost:3000`);
+};
+server.listen(3000, connectHandler);
---
+++ public/index.js
@@ -0,0 +1,5 @@
+const video = document.querySelector("video");
+const btn = document.querySelector("button");
+btn.addEventListener("click", () => {
+  console.log("it is a btn");
+});
